48ee4cfc0ed32f85b599179be33cfa0850088443
“SrisharanVS” <srisharan.psgtech@gmail.com>
2024-10-05 05:17:23 +0530

    Movement animation done

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,42 +1,122 @@
 package main;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
 
-import javax.swing.JPanel;
-import java.awt.Graphics;
+import static utilz.Constants.PlayerConstants.*;
+import static utilz.Constants.Directions.*;
 
 public class GamePanel extends JPanel {
-    private KeyboardInputs keyboardinputs;
-    private MouseInputs mouseinputs;
-    private int xDelta = 100, yDelta = 100;
+
+    private MouseInputs mouseInputs;
+    private float xDelta = 100, yDelta = 100;
+    private BufferedImage img;
+    private BufferedImage[][] animations;
+    private int aniTick, aniIndex, aniSpeed = 15;
+    private int playerAction = IDLE;
+    private int playerDir = -1;
+    private boolean moving = false;
+
     public GamePanel() {
-        MouseInputs mouseInputs = new MouseInputs(this);
+        mouseInputs = new MouseInputs(this);
+        importImg();
+        loadAnimations();
+
+        setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
     }
 
-    public void changeXDelta(int value){
-        this.xDelta += value;
-        repaint();
+    private void loadAnimations() {
+        animations = new BufferedImage[9][6];
+        for (int j = 0; j < animations.length; j++)
+            for (int i = 0; i < animations[j].length; i++)
+                animations[j][i] = img.getSubimage(i * 64, j * 40, 64, 40);
     }
 
-    public void changeYDelta(int value){
-        this.yDelta += value;
-        repaint();
+    private void importImg() {
+        InputStream is = getClass().getResourceAsStream("/player_sprites.png");
+        try {
+            img = ImageIO.read(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public void setRectPos(int x, int y){
-        this.xDelta = x;
-        this.yDelta = y;
-        repaint();
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280, 800);
+        setPreferredSize(size);
     }
 
+    public void setDirection(int direction) {
+        this.playerDir = direction;
+        moving = true;
+    }
+
+    public void setMoving(boolean moving) {
+        this.moving = moving;
+    }
+
+    private void updateAnimationTick() {
+        aniTick++;
+        if (aniTick >= aniSpeed) {
+            aniTick = 0;
+            aniIndex++;
+            if (aniIndex >= GetSpriteAmount(playerAction))
+                aniIndex = 0;
+        }
+
+    }
+
+    private void setAnimation() {
+        if (moving)
+            playerAction = RUNNING;
+        else
+            playerAction = IDLE;
+    }
+
+    private void updatePos() {
+        if (moving) {
+            switch (playerDir) {
+                case LEFT:
+                    xDelta -= 5;
+                    break;
+                case UP:
+                    yDelta -= 5;
+                    break;
+                case RIGHT:
+                    xDelta += 5;
+                    break;
+                case DOWN:
+                    yDelta += 5;
+                    break;
+            }
+        }
+    }
+    public void updateGame() {
+        updateAnimationTick();
+        setAnimation();
+        updatePos();
+    }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-        g.fillRect(xDelta,yDelta ,200,50);
-
+        g.drawImage(animations[playerAction][aniIndex], (int) xDelta, (int) yDelta, 128, 80, null);
     }
+
 }
--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -3,13 +3,19 @@ package main;
 import javax.swing.JFrame;
 
 public class GameWindow {
+    private JFrame jframe;
+
     public GameWindow(GamePanel gamePanel) {
 
-        JFrame jframe = new JFrame();
-        jframe.setSize(400, 400);
+        jframe = new JFrame();
+
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.add(gamePanel);
+        jframe.setLocationRelativeTo(null);
+        jframe.setResizable(false);
+        jframe.pack();
         jframe.setVisible(true);
 
     }
+
 }
